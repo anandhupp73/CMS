010edbfc1d40cc2941fd2e59a9c4b93a95ca6a21
anandhupp73 <anandhakrishnanpp73@gmail.com>
2026-01-28 13:15:38 +0530

    project details and functionalities

--- project/construction/urls.py
+++ project/construction/urls.py
@@ -7,4 +7,12 @@ urlpatterns = [
     path('', views.home, name='construction_home'),
     path('add-project/', views.add_project, name='add_project'),
     path('project_manage/', views.project_manage, name='project_manage'),
+    path('add-phase/<int:project_id>/',views.add_phase,name='add_phase'),
+    path('add-supervisor/<int:project_id>',views.assign_supervisor,name='assign_supervisor'),
+    path('update-progress/<int:project_id>',views.update_progress,name='update_progress'),
+    path('project/<int:project_id>/', views.project_detail, name='project_detail'),
+    path('update-phase/<int:phase_id>/', views.update_phase, name='update_phase'),
+    path('supervisors/', views.supervisors_list, name='pm_supervisors'),
+
+
 ]
--- project/construction/views.py
+++ project/construction/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.contrib.auth import get_user_model
 from django.contrib import messages
-from .models import Project
+from .models import *
+from datetime import timezone
 
 User = get_user_model()
 
@@ -57,3 +58,116 @@ def add_project(request):
 @user_passes_test(is_pm)
 def project_manage(request):
     return render(request, 'construction/project_manage.html')
+
+@login_required
+@user_passes_test(is_pm)
+def add_phase(request, project_id):
+    project = Project.objects.get(id=project_id)
+
+    if request.method == 'POST':
+        phase_name = request.POST.get('phase_name')
+        expected_end = request.POST.get('expected_end')
+
+        if not phase_name or not expected_end:
+            messages.error(request, "All fields are required.")
+            return redirect('add_phase', project_id=project.id)
+
+        ProjectPhase.objects.create(
+            project=project,
+            phase_name=phase_name,
+            expected_end=expected_end
+        )
+
+        messages.success(request, "Phase added successfully.")
+        return redirect('construction:construction_home')
+
+    return render(request, 'construction/add_phase.html', {
+        'project': project
+    })
+
+@login_required
+@user_passes_test(is_pm)
+def assign_supervisor(request, project_id):
+    project = Project.objects.get(id=project_id)
+
+    supervisors = User.objects.filter(role__name='Supervisor', is_active=True)
+
+    if request.method == 'POST':
+        supervisor_id = request.POST.get('supervisor')
+        supervisor = User.objects.get(id=supervisor_id)
+
+        ProjectSupervisor.objects.create(
+            project=project,
+            supervisor=supervisor
+        )
+
+        messages.success(request, "Supervisor assigned")
+        return redirect('construction:construction_home')
+
+    return render(request, 'construction/assign_supervisor.html', {
+        'project': project,
+        'supervisors': supervisors
+    })
+
+@login_required
+@user_passes_test(is_pm)
+def update_progress(request, project_id):
+    project = Project.objects.get(id=project_id)
+
+    phases = project.phases.all()
+
+    if request.method == 'POST':
+        for phase in phases:
+            progress = request.POST.get(f'phase_{phase.id}')
+            if progress:
+                phase.progress = int(progress)
+                phase.save()
+
+        messages.success(request, "Progress updated")
+        return redirect('construction:construction_home')
+
+    return render(request, 'construction/update_progress.html', {
+        'project': project,
+        'phases': phases
+    })
+
+@login_required
+def project_detail(request, project_id):
+    project = Project.objects.get(id=project_id)
+    phases = project.phases.all()
+    supervisors = ProjectSupervisor.objects.filter(project=project)
+
+    return render(request, 'construction/project_detail.html', {
+        'project': project,
+        'phases': phases,
+        'supervisors': supervisors,
+    })
+
+@login_required
+@user_passes_test(is_pm)
+def update_phase(request, phase_id):
+    phase = ProjectPhase.objects.get(id=phase_id)
+
+    if request.method == 'POST':
+        phase.progress = request.POST.get('progress')
+
+        actual_end = request.POST.get('actual_end')
+        phase.actual_end = actual_end if actual_end else None
+
+        phase.save()
+        return redirect('construction:project_detail', phase.project.id)
+
+    return render(request, 'construction/update_phase.html', {
+        'phase': phase
+    })
+
+
+@login_required
+def supervisors_list(request):
+    supervisors = ProjectSupervisor.objects.filter(
+        project__manager=request.user
+    ).select_related('supervisor', 'project')
+
+    return render(request, 'construction/supervisor_list.html', {
+        'supervisors': supervisors
+    })
